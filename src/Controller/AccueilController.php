f0568814401b882e94e76a529c58b9c063d0eaa7
jeremy harel <jeremyharel123@hotmail.com>
2021-09-27 17:06:46 +0200

    "Phase 2 : premier test "hello worlde !"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author jeremy
+ */
+
+
+class AccueilController {
+    
+    /**
+     * @Route("/", name="acceuil")
+     * @return Response
+     */
+    
+    public function index(): Response{
+        return new Response('Hello world !');
+    }
+    //put your code here
+}
